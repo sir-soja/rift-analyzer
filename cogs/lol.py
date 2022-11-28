678acca6b1af53f0ff195681b7091846bbeaa713
soja-n <sir.soja@protonmail.com>
2022-11-29 00:09:51 +0100

    add listing command to list all linked account

--- cogs/lol.py
+++ cogs/lol.py
@@ -7,12 +7,6 @@ class Lol(commands.Cog, name="lol"):
         self.bot = bot
         self.data = DataAccess()
 
-    @commands.command(name="link", description="Link your account")
-    async def link(self, ctx: commands.Context, *text):
-        # need to add some checks on lol id before posting to database
-        print(self.data.new_player(" ".join(text), ctx.author.id))
-        await ctx.send('added to database')
-
 
 async def setup(bot):
     await bot.add_cog(Lol(bot))
