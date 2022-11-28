fd307cf598ff3b30d69597514c25da951778fa9b
sir-soja <sir.soja@protonmail.com>
2022-11-28 11:19:57 +0100

    add commit to db actions

--- data.py
+++ data.py
@@ -7,11 +7,23 @@ class DataAccess:
         self.cursor = self.connector.cursor()
 
     def create_tables(self):
-        return self.cursor.execute('CREATE TABLE players(LOL_ID TEXT, DISCORD_ID);')
+        try:
+            self.cursor.execute('CREATE TABLE players(LOL_ID TEXT, DISCORD_ID);')
+            self.connector.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def new_player(self, lol_id, discord_id):
         query = 'INSERT INTO players(LOL_ID, DISCORD_ID) VALUES (?,?)'
-        return self.cursor.execute(query, (lol_id, discord_id))
+        try:
+            self.cursor.execute(query, (lol_id, discord_id))
+            self.connector.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def get_players(self):
         query = 'SELECT * FROM players'
