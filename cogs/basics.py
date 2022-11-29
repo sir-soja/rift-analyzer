034e8d2abb04b213545a0d6709ca38d7dbd9ef83
sir-soja <sir.soja@protonmail.com>
2022-11-29 11:42:59 +0100

    add a bit of error management and avoid some with checks

--- cogs/basics.py
+++ cogs/basics.py
@@ -11,15 +11,22 @@ class Basics(commands.Cog, name="basics"):
     async def link(self, ctx: commands.Context, *text):
         """Link a LoL account to your Discord profile."""
         # need to add some checks on lol id before posting to database
-        print(self.data.new_player(' '.join(text), ctx.author.id))
-        await ctx.send('added to database')
+        try:
+            self.data.new_player(' '.join(text), ctx.author.id)
+            await ctx.send('added to database')
+        except Exception as e:
+            print(e)
+            await ctx.send('Error with the bot')
 
     @commands.command(name='list', description='List all members.')
     async def list(self, ctx: commands.Context):
         """List all linked account in the server."""
         list_players = self.data.get_players()
-        ui = "".join([f'<@{player[1]}> - {player[0]}\n' for player in list_players])
-        await ctx.send(ui)
+        if list_players:
+            ui = "".join([f'<@{p[1]}> - {p[0]}\n' for p in list_players])
+            await ctx.send(ui)
+        else:
+            await ctx.send('Error with the bot...')
 
     @commands.command(name="ping", description="Get latency of the bot.")
     async def ping(self, ctx: commands.Context):
