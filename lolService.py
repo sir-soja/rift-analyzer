e5ae45e2f7c074715c0db3de6e468146207dafba
sir-soja <sir.soja@protonmail.com>
2022-11-29 12:14:19 +0100

    add a bit of error management and avoid some with checks

--- lolService.py
+++ lolService.py
@@ -7,7 +7,7 @@ class LolService:
     def __init__(self):
         self.path = 'https://euw1.api.riotgames.com/'
         self.summoner_endpoint = 'lol/summoner/v4/summoners/by-name'
-        self.api_token = "RGAPI-766174a9-f467-43c7-8469-01c424b60f15"
+        self.api_token = os.environ["RIOT_TOKEN"]
 
     def get_player_by_name(self, name):
         url = self.path + self.summoner_endpoint
@@ -15,5 +15,3 @@ class LolService:
         if req.status_code == 404:
             return False
         return req.json()
-
-
